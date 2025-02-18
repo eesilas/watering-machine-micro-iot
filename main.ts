333bcc92dc42ea5003e57f44d678980f77931888
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2025-02-18 12:18:03 +0800

    add determination

--- main.ts
+++ main.ts
@@ -11,6 +11,14 @@ microIoT.microIoT_ServoRun(microIoT.aServos.S1, 180)
 basic.pause(500)
 let water_mo = pins.analogReadPin(AnalogPin.P0)
 basic.forever(function () {
-    microIoT.microIoT_showUserNumber(0, Math.trunc(pins.analogReadPin(AnalogPin.P0)))
-    basic.pause(500)
+    if (Math.trunc(pins.analogReadPin(AnalogPin.P0)) < 50) {
+        microIoT.microIoT_showUserNumber(0, Math.trunc(pins.analogReadPin(AnalogPin.P0)))
+        microIoT.microIoT_setIndexColor(PIN.P15, 0, 4, 0x7f00ff)
+        microIoT.microIoT_ServoRun(microIoT.aServos.S1, 90)
+        basic.pause(1000)
+        microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
+        basic.pause(500)
+    } else {
+        microIoT.microIoT_setIndexColor(PIN.P15, 0, 4, 0xff0080)
+    }
 })
